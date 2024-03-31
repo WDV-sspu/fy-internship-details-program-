05f8722e817d6f5b0657cd2315bfba8b96cbb2f7
sj0501819@gmail.com <sj0501819@gmail.com>
2024-03-31 10:22:38 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -17,15 +17,12 @@ import anvil.server
 #   return 42
 @anvil.server.callable
 def submit1(name, prn, address, email, mobile, blood):
-    combine_data = app_tables.personalinfo.get_or_create_row()
-    combine_data.update(name=name, prn=prn, address=address, email=email, mobile=mobile, blood=blood)
+    combine_data = app_tables.personalinfo.get_or_create(name=name, prn=prn, address=address, email=email, mobile=mobile, blood=blood)
 
 @anvil.server.callable
 def submit2(tenth, twelth, cet, jee, sem):
-    combine_data = app_tables.academicinfo.get_or_create_row()
-    combine_data.update(tenth=tenth, twelth=twelth, cet=cet, jee=jee, sem=sem)
+    combine_data = app_tables.academicinfo.get_or_create(tenth=tenth, twelth=twelth, cet=cet, jee=jee, sem=sem)
 
 @anvil.server.callable
 def submit3(certification, linkedin, experience):
-    combine_data = app_tables.expericeneinfo.get_or_create_row()
-    combine_data.update(certification=certification, linkedin=linkedin, experience=experience)
+    combine_data = app_tables.expericeneinfo.get_or_create(certification=certification, linkedin=linkedin, experience=experience)
