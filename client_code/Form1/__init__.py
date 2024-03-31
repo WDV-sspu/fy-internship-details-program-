0fd6bb3a2f3773be0ee704f5e02756ead04af160
sj0501819@gmail.com <sj0501819@gmail.com>
2024-03-31 04:53:07 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -39,5 +39,4 @@ class Form1(Form1Template):
         return  
 
     anvil.server.call('submit1', name=name, prn=prn, address=address, email=email, mobile=mobile, blood=blood)
-
-    open_form('Form2')
+    
