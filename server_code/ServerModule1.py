68de1b0beb5a2a323f08b7a694ca565a24357854
sj0501819@gmail.com <sj0501819@gmail.com>
2024-04-01 14:32:41 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -17,39 +17,5 @@ import anvil.server
 #   return 42
 
 @anvil.server.callable
-def submit1(name, prn, address, email, mobile, blood):
-  # Add a new row to combine_data table
-  combine_data_row = app_tables.combine_data.add_row(
-    name=name,
-    prn=prn,
-    address=address,
-    email=email,
-    mobile=mobile,
-    blood=blood
-  )
-  return combine_data_row
-
-@anvil.server.callable
-def submit2(tenth, twelth, cet, jee, sem):
-    # Get the existing row from combine_data table
-    combine_data_id = app_tables.combine_data.get_by_id()
-    # Update the row with data from submit2
-    combine_data_row.update(
-        tenth=tenth,
-        twelth=twelth,
-        cet=cet,
-        jee=jee,
-        sem=sem
-    )
-
-@anvil.server.callable
-def submit3(certification, linkedin, experience, row_id):
-  # Get the existing row from combine_data table
-  combine_data_row = app_tables.combine_data
-
-  # Update the row with data from submit3
-  combine_data_row.update(
-    certification=certification,
-    linkedin=linkedin,
-    experience=experience
-  )
+def submit1( name, prn, address, email, mobile, blood):
+  app_tables.combine_data.add_row(name=name, prn=prn, mobile=mobile, email=email, blood=blood)
