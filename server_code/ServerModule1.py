2f9504840f96ceac04d2e399ae305798e2fa94e3
sj0501819@gmail.com <sj0501819@gmail.com>
2024-04-18 11:02:39 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,4 @@
+import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -21,3 +22,7 @@ def submit1(name, prn, email, mobile, address, blood, tenth, twelth, jee, cet, s
         linkedin=linkedin,
         work_experience=work_experience
     )
+    anvil.email.send(from_name = "My App Support", 
+                 to = "email",
+                 subject = "Welcome",
+                 text = "Welcome to My App!")
